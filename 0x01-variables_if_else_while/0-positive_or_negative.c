687bb3400a4f44733dfa78b1f47971f5fd5ea596
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-09 12:39:21 -0700

    0x01-variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,24 @@
 int main() {
 	    double num;
 
-	        printf("$RANDOM");
+	        printf($RANDOM);
 
 		    scanf("%lf", &num);
 
 		        if (num <= 0.0) {
 				     if (num == 0.0)
 			printf("is zero");
+				     
 					else
+
 			printf("is negative");
+
    } 
+
 				    else
+
 			printf("is positive");
+
 			    	return (0);
 
 }
