63ce4c52729e22f0f1d7962ba7a751a39dedfdd6
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-09 12:27:20 -0700

    0x00-hello-world

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	    double num;
+
+	        printf("$RANDOM");
+
+		    scanf("%lf", &num);
+
+		        if (num <= 0.0) {
+				     if (num == 0.0)
+			printf("is zero");
+					else
+			printf("is negative");
+   } 
+				    else
+			printf("is positive");
+			    	return (0);
+
+}
