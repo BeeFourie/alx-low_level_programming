41987f06b52cb73a3ea73ee6937e35b3a5edad22
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-09 09:59:46 -0700

    0x00-hello-world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash | #include <stdio.h> | #include <string.h>
-int main () {char str1[15] char str2[15];strcpy(str1, "Programming is like building a multilingual puzzle");strcpy(sr2, );puts(str1);puts(str2);return(0);}
+int main () {char str1[15] char str2[15];strcpy(str1, "Programming is like building a multilingual puzzle");puts(str1);return(0);}
