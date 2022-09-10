1dca736e580cea735826c6a3ec72c6ff1cb6712f
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-10 01:14:41 -0700

    0x01-variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'assign a random number to the variable'
+ *
+ * Return Always 0 (success)
+ */
 
 int main() 
 {
-	    double num;
-
-	        printf("n");
-
-		    scanf("%lf", &num);
-
-		        if (num <= 0.0) {
-				     if (num == 0.0)
-			printf("is zero");
-				     
-					else
-
-			printf("is negative");
-
-   } 
-
-				    else
-
-			printf("is positive");
-
-			    	return (0);
-
+double num;
+printf("n");
+scanf("%lf", &num);
+if (num <= 0.0) {
+if (num == 0.0)
+printf("is zero");
+else
+printf("is negative");
+} 
+else
+printf("is positive");
+return (0);
 }
