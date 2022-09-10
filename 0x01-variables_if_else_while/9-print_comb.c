21bc4a39b2836a22dd321217206e5378b70563e8
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-10 00:28:57 -0700

    0x01-variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
-
-
 int main(void)
 
 {
-
-    int (x);
-
-
-	        for (x = 0; x < 10; x++)
-
-			    {
-
-			        putchar(x + '0');
-
-				            if (x < 9)
-
-			 {
-
-									                putchar(',');
-
-											            putchar(' ');
-
-												            }
-
-						        }
-
-		    putchar('\n');
-
-		        return (0);
-
+int (x);
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0');
+if (x < 9)
+{								                putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
