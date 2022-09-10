a2023aa6ab23681d8e7c58ae56e1bb0a231e5289
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-10 00:42:47 -0700

    0x01-variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: 'program that prints all possible combinations of single-digit numbers'
+ * Description: 'print all possible combinations of single-digit numbers'
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
