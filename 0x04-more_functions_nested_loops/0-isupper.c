abc74dd8899cda84a182f796defa9618e95a9c65
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-15 07:18:29 -0700

    0x04-more_functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c;
+int _isupper(int c);
+scanf("%c", &c);
+
+if(isupper(ch))
+}
