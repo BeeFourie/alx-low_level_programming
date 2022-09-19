5b325f441e38137b836e00e5007912b4f14499e2
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-19 09:12:04 -0700

    0x05-pointers_arrays_

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
-void reset_to_98(int, "98");
+#include <stdio.h>
+void function(int* a)
+{
+(*a)++;
+printf("(*a) = %d\n",(*a) ); 
+}
+int main(void)
+{
+int x = 98;
+int *p = &x;
+printf("x = %d\n", x );
+function(p);
+return (0);
+}
