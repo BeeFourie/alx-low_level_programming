f6fa37a3eed8f9e39ef42a55ab1beba50b763af2
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-22 08:29:32 -0700

    0x00-hello_

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
  * Description: 'size of diffrent data types'
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 int integerType;
 float floatType;
 double doubleType;
 char charType;
-printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-printf("Size of a long int: %ld byte(s)\n", sizeof(integerType));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(doubleType));
-printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+printf( "Size of a char: %ld byte(s)\n", sizeof(charType));
+printf( "Size of an int: %ld byte(s)\n", sizeof(integerType));
+printf( "Size of a long int: %ld byte(s)\n", sizeof(integerType));
+printf( "Size of a long long int: %ld byte(s)\n", sizeof(doubleType));
+printf( "Size of a float: %ld byte(s)\n", sizeof(floatType));
 fprintf(stderr, "");
 return(0);
 }
