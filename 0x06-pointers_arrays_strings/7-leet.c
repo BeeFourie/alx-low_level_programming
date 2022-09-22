6e89b159a59d133b4435fe6cb568f957f067c6c9
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-22 14:18:17 -0700

    0x06-pointers_arrays

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,11 @@
+#include <main.h>
+#include <stdio.h>
+/**
+* main - check the code for
+* Return: Always 0.
+*/
+int main(void)
+{
+output = str_replace(array('a','A','e','E','o','O','t','T','l','L'), array('4','4','3','3','0','0','7','7','1','1'), $str);
+return (0);
+}
