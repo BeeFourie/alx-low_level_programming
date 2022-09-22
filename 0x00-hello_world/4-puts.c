5b0b1927d08f1936bae0ffdb1dd913411cff926e
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-22 07:31:27 -0700

    0x00-hello_

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash | #include <stdio.h> | #include <string.h>
-int main () {char str1[15] char str2[15]; put(str1, "Programming is like building a multilingual puzzle");puts(str1);return(0);}
+#include <stdio.h> 
+#include <string.h>
+int main (void) 
+{
+char str1[15]; 
+puts(char "Programming is like building a multilingual puzzle\n");
+puts(str1);
+return(0);
+}
