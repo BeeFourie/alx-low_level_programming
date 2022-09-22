a3da7572227770dfb4676a7c9a658039a345590e
Phebe Fourie <WolfGriefs@gmail.com>
2022-09-22 08:41:25 -0700

    0x00-hello_

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ double doubleType;
 char charType;
 printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-printf("Size of a long int: %ld byte(s)\n", sizeof(integerType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(doubleType));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 fprintf(stderr, "");
